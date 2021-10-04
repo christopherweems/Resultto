b0667e7347258ce42c1ae1db5bfe84d840b2e0f8
Christopher Weems <hello@christopherweems.com>
2021-10-04 12:02:07 -0400

    Remove unnecessary "import Foundation" declarations

--- Sources/Resultto/AllTrue.swift
+++ Sources/Resultto/AllTrue.swift
@@ -1,8 +1,6 @@
 //  AllTrue.swift
 //  1/1/21
 
-import Foundation
-
 @resultBuilder
 public struct AllTrue {
     public static func buildBlock(_ elements: Bool...) -> Bool {
--- Sources/Resultto/Count.swift
+++ Sources/Resultto/Count.swift
@@ -1,8 +1,6 @@
 //  Count.swift
 //  Created by Christopher Weems on 2/23/21
 
-import Foundation
-
 @resultBuilder
 public struct Count {
     public static func buildBlock(_ elements: Bool...) -> Int {
